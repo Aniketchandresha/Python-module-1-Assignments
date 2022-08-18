4319d9469adc2aa2184ef1e6b4aa119b0b5c9e23
Aniketchandresha <aniketchandresha@gmail.com>
2022-08-18 19:06:26 +0530

    reverse string

--- Module-1-Assignments/1/max_length_string.py
+++
@@ -1,4 +0,0 @@
-'''Write a Python function that takes a list of words and returns the length 
-of the longest one.
-'''
-
---
+++ Module-1-Assignments/reverse_strin.py
@@ -0,0 +1,4 @@
+# Write a Python function to reverses a string if its length is a multiple of 4.
+
+string = input("Enter any name : ")
+print (string[::-1 ])
